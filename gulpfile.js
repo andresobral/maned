7a439668f91befd2c5cb9458d0cefe6056407079
Andre Sobral <andreqsobral@gmail.com>
2015-09-13 21:18:26 -0300

    Maned basic build structure

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var concat = require('concat');
+var uglify = require('uglify');
+var rename = require('rename');
+var order = require('order');
+
+gulp.task('build', function() {
+  gulp.src('./src/**/*.js')
+    .pipe(order([
+      "src/Preloader.js",
+      "src/Maned.js"
+    ]))
+    .pipe(concat("maned.js"))
+    .pipe(uglify())
+    .pipe(rename("maned.min.js"))
+    .pipe(gulp.dest("./dist"))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.js', ['build']);
+});
+
+gulp.task('default', ['watch']);
