5ba3d542d8ba3445292101d043c397e4ce8977fb
Andre Sobral <andreqsobral@gmail.com>
2016-02-23 21:29:16 -0300

    improvements in the snake game

--- games/snake/app.js
+++ games/snake/app.js
@@ -1,117 +1,96 @@
+var game;
+var grid;
+var snake;
+
+var nodeSize = 20;
+
+function Grid(direction, cols, rows) {
+  this.direction = direction;
+  this.rows = rows;
+  this.cols = cols;
+  this.arr = [];
+
+  this.init();
+}
+
+Grid.prototype.init = function() {
+  console.log('Grid inited...');
+  for(var i  = 0; i < this.cols; i++) {
+    this.arr.push([]);
+    for(var j = 0; j < this.rows; j++) {
+      this.arr[i].push(0);
+    }
+  }
+};
+
+Grid.prototype.setEntity = function(value, x, y) {
+  this.arr[x][y] = value;
+};
+
+Grid.prototype.getEntity = function(x, y) {
+  return this.arr[x][y];
+};
+
+function Snake(direction, rows, cols) {
+  this.direction = direction;
+  this.queue = [];
+  this.insert(rows, cols);
+}
+
+Snake.prototype.insert = function(x, y) {
+  this.queue.unshift({x: x, y: y});
+  this.last = this.queue[0];
+}
+
+Snake.prototype.remove = function() {
+  return this.queue.pop();
+}
+
+function render() {
+  for(var i = 0; i < grid.cols; i++) {
+    for(var j = 0; j < grid.rows; j++) {
+      switch(grid.getEntity(i, j)) {
+        case 0:
+          game.ctx.fillStyle = "#fff";
+          break;
+        case 1:
+          game.ctx.fillStyle = "blue";
+          break;
+      }
+
+      game.ctx.fillRect(i * nodeSize, j * nodeSize, nodeSize, nodeSize);
+    }
+  }
+}
+
+game = new Maned({
+  className: '.engine',
+  width: 400,
+  height: 320,
+  background: '#fff',
+  fps: 5
+});
+
+game.on('preload', function() {
+  game.preload([]);
+});
+
+game.on('start', function() {
+  console.log('start...');
+  grid = new Grid('right', 20, 16);
+  snake = new Snake('right', 10, 8);
+  grid.setEntity(1, 10, 8);
+
+  console.log(grid);
+});
+
+game.on('update', function() {
+  // console.log('update...');
+  render();
+});
+
 window.onload = function() {
 
-  var game = new Maned({
-    className: '.engine',
-    width: 400,
-    height: 320,
-    background: '#fff',
-    fps: 5
-  });
-
-  var grid;
-  var snake;
-  var nodeSize = 20;
-  var snakeSpeed = 20;
-
-  var score = 0;
-
-  game.on('preload', function() {
-    game.preload([]);
-  });
-
-  game.on('start', function() {
-    grid = new Grid(game.height / nodeSize, game.width / nodeSize);
-    snake = new Snake ('right', 10, 8);
-  });
-
-  game.on('update', function() {
-    if(game.controls.isPressedKey('ARROW_UP')) {
-      // direction = 'up';
-      // moveSnake();
-    }
-
-    if(game.controls.isPressedKey('ARROW_RIGHT')) {
-      // direction = 'right';
-      // moveSnake();
-    }
-
-    if(game.controls.isPressedKey('ARROW_DOWN')) {
-      // direction = 'down';
-      // moveSnake();
-    }
-
-    if(game.controls.isPressedKey('ARROW_LEFT')) {
-      // direction = 'left';
-      // moveSnake();
-    }
-
-    if(game.controls.isPressedKey('S')) {
-      console.log("snake nodes: " + snake.length);
-      console.log(snake);
-    }
-
-    // render();
-  });
-
   game.init();
-
-  function Grid(cols, rows) {
-    this.cols = cols;
-    this.rows = rows;
-    this.arr = [];
-    console.log(this);
-
-    for(var i = 0; i < cols; i++) {
-      this.arr.push([]);
-      for(var j = 0; j < cols; j++) {
-        this.arr[i].push(0);
-      }
-    }
-  }
-
-  Grid.prototype.getEntity = function(x, y) {
-    // console.log(x + ' - ' + y);
-    return this.arr[x][y];
-  };
-
-  function Snake(direction, col, row) {
-    this.direction = direction;
-    this.queue = [];
-    console.log(this);
-  }
-
-  Snake.prototype.insert = function(row, col) {
-    this.queue.unshift({
-      x: transformPositionX(row),
-      y: transformPositionX(col)
-    });
-    this.last = this.queue[0];
-  };
-
-  function transformPositionX(row) {
-    return row * nodeSize;
-  }
-
-  function transformPositionY(col) {
-    return col * nodeSize;
-  }
-
-  function render() {
-    for(var i = 0; i < grid.rows; i++) {
-      for(var j = 0; j < grid.cols; j++) {
-        // grid.getEntity(i, j);
-        console.log(grid.getEntity(i, j));
-        switch(grid.getEntity(i, j)) {
-          case 0:
-            game.ctx.fillStyle = "#fff";
-            break;
-          case 1:
-            game.ctx.fillStyle = "blue";
-            break;
-        }
-        game.ctx.fillRect(i * nodeSize, j * nodeSize, nodeSize, nodeSize);
-      }
-    }
-  }
 
 };
