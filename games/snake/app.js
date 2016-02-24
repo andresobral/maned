6555812b3dfb18cd7c349b2843834e6a4750c5db
Andre Sobral <andreqsobral@gmail.com>
2016-02-24 16:48:34 -0300

    working snake game

--- games/snake/app.js
+++ games/snake/app.js
@@ -1,6 +1,7 @@
 var game;
 var grid;
 var snake;
+var score = 0;
 
 var nodeSize = 20;
 
@@ -14,7 +15,6 @@ function Grid(direction, cols, rows) {
 }
 
 Grid.prototype.init = function() {
-  console.log('Grid inited...');
   for(var i  = 0; i < this.cols; i++) {
     this.arr.push([]);
     for(var j = 0; j < this.rows; j++) {
@@ -56,11 +56,46 @@ function render() {
         case 1:
           game.ctx.fillStyle = "blue";
           break;
+        case 2:
+          game.ctx.fillStyle = "orange";
+          break;
       }
 
       game.ctx.fillRect(i * nodeSize, j * nodeSize, nodeSize, nodeSize);
     }
   }
+
+  updateScore();
+}
+
+function createFood() {
+  var empty = [];
+
+  for(var i = 0; i < grid.cols; i++) {
+    for(var j = 0; j < grid.rows; j++) {
+      if(grid.getEntity(i, j) === 0) {
+        empty.push({x: i, y: j});
+      }
+    }
+  }
+
+  var position = empty[Math.round(Math.random()*(empty.length - 1))];
+  grid.setEntity(2, position.x, position.y);
+}
+
+function updateScore() {
+  game.text(score, game.width/2, 20, {
+    font: "18px Arial",
+    color: "#000",
+    textAlign: "center"
+  });
+}
+
+function init() {
+  grid = new Grid('right', 20, 16);
+  snake = new Snake('right', 10, 8);
+  grid.setEntity(1, 10, 8);
+  createFood();
 }
 
 game = new Maned({
@@ -77,15 +112,59 @@ game.on('preload', function() {
 
 game.on('start', function() {
   console.log('start...');
-  grid = new Grid('right', 20, 16);
-  snake = new Snake('right', 10, 8);
-  grid.setEntity(1, 10, 8);
-
-  console.log(grid);
+  init();
 });
 
 game.on('update', function() {
-  // console.log('update...');
+  if(game.controls.isPressedKey('ARROW_UP')) {
+    snake.direction = 'up';
+  }
+
+  if(game.controls.isPressedKey('ARROW_RIGHT')) {
+    snake.direction = 'right';
+  }
+
+  if(game.controls.isPressedKey('ARROW_DOWN')) {
+    snake.direction = 'down';
+  }
+
+  if(game.controls.isPressedKey('ARROW_LEFT')) {
+    snake.direction = 'left';
+  }
+
+  var x = snake.last.x;
+  var y = snake.last.y;
+
+  switch(snake.direction) {
+    case 'left':
+      x--;
+      break;
+    case 'up':
+      y--;
+      break;
+    case 'right':
+      x++;
+      break;
+    case 'down':
+      y++;
+      break;
+  }
+
+  if(x < 0 || x > grid.cols - 1 || y < 0 || y > grid.rows - 1 || grid.getEntity(x, y) === 1) {
+    return init();
+  }
+
+  if(grid.getEntity(x, y) === 2) {
+    score += 100;
+    createFood();
+  } else {
+    var tail = snake.remove();
+    grid.setEntity(0, tail.x, tail.y);
+  }
+
+  grid.setEntity(1, x, y);
+  snake.insert(x, y);
+
   render();
 });
 
